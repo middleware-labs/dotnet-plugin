0cc1a70d07fe87fbf72afca8e4d45d7f8e7608f9
parth-dadhaniya <parth@middleware.io>
2023-07-29 16:31:35 +0530

    [dotnet-plugin]: Added the dotnet plugin

---
+++ DotnetMwPlugin/DotnetPlugin.cs
@@ -0,0 +1,21 @@
+﻿using OpenTelemetry.Resources;
+
+namespace DotnetMwPlugin;
+
+public class DotnetPlugin
+{
+    public void Initializing()
+    {
+        Console.WriteLine("Initializing DotnetPlugin");  
+    }
+    
+    public ResourceBuilder ConfigureResource(ResourceBuilder builder)
+    {
+        builder.AddAttributes(new[]
+        {
+            new KeyValuePair<string, object>("mw.app.lang", "dotnet"),
+            new KeyValuePair<string, object>("runtime.metrics.dotnet", "true")
+        });
+        return builder;
+    }
+}
---
+++ DotnetMwPlugin/Logger/Logger.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+
+namespace DotnetMwPlugin.Logger;
+
+public class Logger
+{
+    private static readonly ServiceProvider ServiceProvider = new ServiceCollection()
+        .AddLogging(
+            loggingBuilder => loggingBuilder
+                .SetMinimumLevel(LogLevel.Warning)
+                .AddOpenTelemetry(
+                    options => options.AddConsoleExporter()
+                    )
+        )
+        .BuildServiceProvider();
+    
+    private static readonly ILogger LoggerInstance = ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger<Logger>();
+
+    private Logger()
+    {
+        
+    }
+
+    public static void Info(string log)
+    {
+        Log(log, LogLevel.Information);
+    }
+    
+    public static void Warning(string log)
+    {
+        Log(log, LogLevel.Warning);
+    }
+    
+    public static void Debug(string log)
+    {
+        Log(log, LogLevel.Debug);
+    }
+    
+    public static void Error(Exception exception)
+    {
+        Log(exception);
+    }
+    
+    private static void Log(string log, LogLevel logLevel)
+    {
+        LoggerInstance.Log(logLevel, "{Log}", log);
+    }
+    
+    private static void Log(Exception exception)
+    {
+        LoggerInstance.LogError(exception, "{ExceptionMessage}", exception.Message);
+    }
+}
