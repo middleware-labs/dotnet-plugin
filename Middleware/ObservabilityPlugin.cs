9e0f3b5453cd05a2bf1189ba86eecc2a247b83d4
parth-dadhaniya <parth@middleware.io>
2023-07-29 16:31:35 +0530

    [dotnet-plugin]: Added the dotnet plugin

---
+++ Middleware/ObservabilityPlugin.cs
@@ -0,0 +1,21 @@
+﻿using OpenTelemetry.Resources;
+
+namespace Middleware;
+
+public class ObservabilityPlugin
+{
+    public void Initializing()
+    {
+        Console.WriteLine("Initializing ObservabilityPlugin");  
+    }
+    
+    public ResourceBuilder ConfigureResource(ResourceBuilder builder)
+    {
+        builder.AddAttributes(new[]
+        {
+            new KeyValuePair<string, object>("mw.app.lang", "dotnet"),
+            new KeyValuePair<string, object>("runtime.metrics.dotnet", "true")
+        });
+        return builder;
+    }
+}
